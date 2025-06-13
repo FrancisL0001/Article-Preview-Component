13906558dcc76ac7390f16c9a66ab451d8416e88
Francois LEUTOU <francoisleuts@gmail.com>
2025-06-13 07:28:55 +0100

    Mobile active state added.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const button = document.getElementById("Share");
+const footer = document.getElementById("foot");
+const footerImg = footer.querySelector('img');
+const footerP = footer.querySelector('p');
+const button = footer.querySelector('button');
 
-button.addEventListener("click", function() {
-    const footer = document.getElementsByClassName("footer");
-    footer.style.display = none;
-    footer.style.backgroundColor = hsl(217, 19, 35);
-    console.log("SHARE");
-});
+button.addEventListener("click", function(e) {
+    const test = footer.classList.toggle('activefooter');
+    if (test){   
+        footer.innerHTML = `
+           <p> SHARE </p>
+           <div class='social-icons'>
+            <img src = './images/icon-facebook.svg' alt = 'facebook'>
+            <img src = './images/icon-twitter.svg' alt = 'facebook'>
+            <img src = './images/icon-pinterest.svg' alt = 'facebook'>
+           </div>
+        `;
+        footer.appendChild(button)
+    }else{
+        footer.innerHTML = `
+            <img src="./images/avatar-michelle.jpg" alt="Michelle Appleton" class="avatar" id="avatar">
+            <p><strong>Michelle Appleton</strong>  <br>28 Jun 2020</p>
+
+        `
+        footer.appendChild(button)
+    };
+})
